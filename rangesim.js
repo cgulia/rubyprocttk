7465b38e32d1b517f5caab85ff0165fa59b9c644
ulia <101744917+cgulia@users.noreply.github.com>
2023-06-22 21:24:16 +0200

    Update rangesim.js
    - function split
    - roll+stat based sim
    - data formatting
    - data export (fs)

--- rangesim.js
+++ rangesim.js
@@ -1,4 +1,5 @@
-const iterations = 200000;
+const fs = require('fs');
+const iterations = 10000;
 const gearacc = 245;
 const gearstr = 25;
 const rubytoggle = true;
@@ -16,14 +17,40 @@ const weapons = {
         str:122,
         spd:5,
         ruby:true,
+    },
+    bofa: {
+        acc:221,
+        str:113,
+        spd:5,
+        ruby:false,
     }
 };
+
+const armour = {
+    masoribuckler: {
+        acc:135,
+        str:25,
+    },
+    masori: {
+        acc:117,
+        str:15,
+    },
+    dhides: {
+
+    }
+}
 
 const target = {
     zebak: {
         hp:580,
         bdef:70,
         rdef:110,
+        toamult:true,
+    },
+    wardenp3: {
+        hp:880,
+        bdef:150,
+        rdef:20,
         toamult:true,
     },
     olm: {
@@ -39,8 +66,6 @@ const target = {
         toamult:false,
     }
 };
-
-var out = [];
 
 calc(target.zebak, weapons.zcb);
 function calc(trg,wpn) {
@@ -58,8 +83,22 @@ function calc(trg,wpn) {
     const dph = (max * acc) / 2;
     const dps = dph / 3;
     console.log('max:' + max + '\nattackroll:' + aroll + '\ndefroll:' + droll + '\nacc:' + acc + '\ndph:' + dph + '\ndps:' + dps);
-    console.log('\nrunning ' + iterations + ' iterations...');
-    for (let it = 0; it < iterations; it ++) {
+}
+
+var out = [];
+sim(target.zebak, 48822, 53, 5, 10000);
+
+function sim(trg, maroll, max, aspd, its) {
+    //npc max def roll
+    var mdroll = 0;
+    if (trg.toamult) {
+        mdroll = Math.floor(((trg.bdef + 9) * (trg.rdef + 64) * drollmult));
+    } else {
+        mdroll = Math.floor((trg.bdef + 9) * (trg.rdef + 64));
+    }
+
+    console.log('\nrunning ' + its + ' iterations...');
+    for (let it = 0; it < its; it ++) {
         var chp = 0;
         if (trg.toamult) {
             chp = (trg.hp * drollmult);
@@ -70,7 +109,8 @@ function calc(trg,wpn) {
         var attackcount = 0 + zcbspecs;
         while (chp > 0) {
             var cdmg = 0;
-            
+
+
             if (rubytoggle) {
                 var rubyroll = Math.floor(Math.random() * 1000);
             } else {
@@ -83,8 +123,8 @@ function calc(trg,wpn) {
                     cdmg = 110;
                 }
             } else {
-                var caroll = Math.floor(Math.random() * aroll);
-                var cdroll = Math.floor(Math.random() * droll);
+                var caroll = Math.floor(Math.random() * maroll);
+                var cdroll = Math.floor(Math.random() * mdroll);
                 if (caroll > cdroll) {
                     cdmg = Math.floor(Math.random() * max);
                 } else {
@@ -96,12 +136,52 @@ function calc(trg,wpn) {
             attackcount += 1;
         }
 
-        out.push(attackcount * 3);
+        out.push(Math.floor(attackcount * (0.6 * aspd)));
     }
+    printout();
+}
 
+function printout() {
+    out.sort(function(a, b){return a - b});
     var atotal = 0;
     for (let a = 0; a < out.length; a++) {
         atotal += out[a];
     }
-    console.log('ttk average after ' + iterations + ' iterations: ' + atotal / out.length);
+    console.log('ttk average after ' + iterations + ' iterations: ' + atotal / out.length); 
+    dataexport();
+}
+
+function dataexport() {
+    let data = []
+    for (let l = 0; l < out.length; l++) {
+        if (l == 0) {
+            data.push([out[0], 1]);
+        } else {
+            if (data[data.length - 1][0] == out[l]) {
+                data[data.length - 1][1] += 1;
+            } else {
+                data.push([out[l], 1]);
+            }
+        }
+    }
+
+    var labels = '';
+    var values = '';
+    for (let d = 0; d < data.length; d++) {
+        labels += "'" + data[d][0] + "',";
+        values += data[d][1] + ',';
+        //console.log(data[d][0] + ' ' + data[d][1]);
+    }
+
+    try {
+        const file = fs.writeFileSync('labels.txt', labels, { flag: 'w+'})
+    } catch (err) {
+        console.error(err)
+    }
+
+    try {
+        const file = fs.writeFileSync('values.txt', values, { flag: 'w+'})
+    } catch (err) {
+        console.error(err)
+    }
 }
